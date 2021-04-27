1f3c2bb5df69039161c5012ff349d8efeb75c99e
Margaret <match14@yandex.ru>
2021-04-28 00:48:58 +0300

    part adaptive for main page

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
-import {$} from './common';
+import {$, Inputmask} from './common';
 
-// $(window).scroll(function(){
-// 	if($(this).scrollTop()>300){
-// 		$('.js-move-up').addClass('visible');
-// 	}else{
-// 		$('.js-move-up').removeClass('visible');
-// 	}
-// });
-// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// move-up
+$(window).on('scroll', function(){
+	if($(this).scrollTop()>300){
+		$('.js-move-up').addClass('visible');
+	}else{
+		$('.js-move-up').removeClass('visible');
+	}
+});
+$('.js-move-up').on('click', function(){$('body,html').animate({scrollTop:0},800);return false;});
 
 // gallery slider
 if($('.js-gal-slider').length){
@@ -26,5 +27,17 @@ if($('.js-partners-list').length){
   		slidesToScroll: 4,
 		prevArrow: '<button id="prev" type="button" class="btn-arr btn-arr_left"><svg class="icon ic-arrow-left" width="16" height="24"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-left"></use></svg></button>',
 		nextArrow: '<button id="next" type="button" class="btn-arr btn-arr_right"><svg class="icon ic-arrow-right" width="16" height="24"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-right"></use></svg></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+		]
 	});
 }
+
+// Phone mask
+Inputmask('+7 (999) 999-9999').mask('.js-phone');
